aeaa06254087a8e4ff9a3a3552f9007266a7b205
Raphael Melo <raphaelmelo@users.noreply.github.com>
2020-04-10 13:49:21 -0300

    Resposta do Ex.4

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -14,7 +14,7 @@ valorAposInvestimento que é multiplicado por uma taxa de juros de acordo com o 
 Ao ser chamada a função retorna (return) o valor da variavel valorAposInvestimento
 
 
-3.Nesse exercicíos temos 3 variaveis no escopo global que recevem arrays como valor. A primeira é a numeros que recebe u array de numeros e as demais tem
+3. Nesse exercicíos temos 3 variaveis no escopo global que recevem arrays como valor. A primeira é a numeros que recebe u array de numeros e as demais tem
 arrays vazios. Logo após temos um loop do tipo for of que tem como parametros os elementos do array numeros. Dentro do loop temos um if que a cada rodada
 do for of verifica se o numero é par, se for ele atribui a variavel array1. Senão ele atribui ao array2.
 
@@ -22,4 +22,11 @@ Por fim temos o primeiro console.log que exibe no
 console.log uma string concatenada com o o numero de elementos do array numeros. O segundo imprimi o tatal de arrays do array1 e o segundo faz o mesmo quanto
 ao array2. 
 
-*/
+
+4. Nesse trecho de código temos uma const que recebe um array de numeros e duas lets que uma recebe como valor infinito e outra recebe 0. Um loop for of que
+compara se os elementos do array Numeros é menor que o infinito, se sim, atribue ao esse valor. E outro if que verifica os elementos do array de Numeros se 
+algum é maior que o valor da variavel numero2; 
+
+Por fim ele vai imprimir no console duas vezes. Primeiro> 1590 | E depois: -10
+
+*/git commit -m"Resposta do Ex.4"
