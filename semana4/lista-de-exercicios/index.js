c1699714f8adc7e83058f0a040bb3a95c26d2d5b
Raphael Melo <raphaelmelo@users.noreply.github.com>
2020-04-10 12:53:06 -0300

    Resposta do Ex.1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+/*
+1. No código temos uma função que deve receber como parametro() um number. Desntro do escopo da função temos uma const que trata o parametro recebido,
+só aceitando se for number. E Posteriormente temos um return que concatena uma string com a multiplicação da conta entre o valor em dolar e cotaçao.
+
+Por fim atribui a uma variavel a função e a chama com o parametro(100).
+
+No console o resultado será o valor da cotação (que o usuario deve digitar no promp) x o valor do parametro dado a função. 
+
+2. 
+
+
+
+
+*/
