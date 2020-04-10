3e165dd8dedaf628fbefe36184202ae427448ac4
Raphael Melo <raphaelmelo@users.noreply.github.com>
2020-04-10 13:04:48 -0300

    Resposta do Ex.2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,9 +6,10 @@ Por fim atribui a uma variavel a função e a chama com o parametro(100).
 
 No console o resultado será o valor da cotação (que o usuario deve digitar no promp) x o valor do parametro dado a função. 
 
-2. 
+2. No codigo temos uma função nomeada que recebe dois parametros (tipoDeInvestimento, valor). No escopo dessa função temos uma variavel chamada 
+valorAposInvestimento que não recebe nada. Na função um loop do tipo switch confere se um dos caso é true, caso seja ele atribui um valor a variavel
+valorAposInvestimento que é multiplicado por uma taxa de juros de acordo com o caso em que caia. No fim do loop ele tem um default, que retorna uma alert.
 
-
-
+Ao ser chamada a função retorna (return) o valor da variavel valorAposInvestimento
 
 */
