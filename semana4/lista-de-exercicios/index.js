d636e23a82c5b042e7a3fbfc52a34441c8ae8baa
Raphael Melo <raphaelmelo@users.noreply.github.com>
2020-04-10 13:32:27 -0300

    Resposta do Ex.3

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,10 +6,20 @@ Por fim atribui a uma variavel a função e a chama com o parametro(100).
 
 No console o resultado será o valor da cotação (que o usuario deve digitar no promp) x o valor do parametro dado a função. 
 
+
 2. No codigo temos uma função nomeada que recebe dois parametros (tipoDeInvestimento, valor). No escopo dessa função temos uma variavel chamada 
 valorAposInvestimento que não recebe nada. Na função um loop do tipo switch confere se um dos caso é true, caso seja ele atribui um valor a variavel
 valorAposInvestimento que é multiplicado por uma taxa de juros de acordo com o caso em que caia. No fim do loop ele tem um default, que retorna uma alert.
 
 Ao ser chamada a função retorna (return) o valor da variavel valorAposInvestimento
+
+
+3.Nesse exercicíos temos 3 variaveis no escopo global que recevem arrays como valor. A primeira é a numeros que recebe u array de numeros e as demais tem
+arrays vazios. Logo após temos um loop do tipo for of que tem como parametros os elementos do array numeros. Dentro do loop temos um if que a cada rodada
+do for of verifica se o numero é par, se for ele atribui a variavel array1. Senão ele atribui ao array2.
+
+Por fim temos o primeiro console.log que exibe no
+console.log uma string concatenada com o o numero de elementos do array numeros. O segundo imprimi o tatal de arrays do array1 e o segundo faz o mesmo quanto
+ao array2. 
 
 */
