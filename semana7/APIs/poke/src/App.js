c11e614dfe106ec1809d0edd692ae0788fd5bf3e
raphaelmelo <contatowbm@gmail.com>
2020-05-02 20:39:26 -0300

    Logica da Integração da API

--- semana7/APIs/poke/src/App.js
+++ semana7/APIs/poke/src/App.js
@@ -3,44 +3,52 @@ import axios from "axios";
 
 class App extends React.Component {
   state = {
-    pokerList: [],
-    pokeImage: "",
-    pokeType: ""
+    pokeList: [],
+    pokeImage: ""
   };
 
   componentDidMount = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=200").then(response => {
-      this.state({ pokerList: response.data.results });
-    }).catch(err => {
-      console.log(err)  
-    });
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        this.setState({ pokeList: response.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   pegaPokemon = event => {
-    const pokeName = event.target.value
-
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then(response => {
-      console.log(response.data.sprites.front_default);
-      this.setState({ pokeImage: response.data.sprites.front_default })
-    }).catch(err => {
-      console.log(err)
-    })
-    
+    const pokeName = event.target.value;
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then(response => {
+        console.log(response.data.sprites.front_default);
+        this.setState({ pokeImage: response.data.sprites.front_default });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
     return (
       <div className="App">
         <select onChange={this.pegaPokemon}>
-            <option value={""}>Nenhum</option>
-            {this.state.pokerList.map(pokemon => {
-              return (
-                <option key={pokemon.name} value={pokemon.name}>
-                  {pokemon.nome}
-                </option>
-              )
-            })}
+          <option value={""}>Nenhum</option>
+          {this.state.pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
         </select>
+        {this.state.pokeImage && (
+          <div>
+            <img src={this.state.pokeImage} alt="pokemon" />
+          </div>
+        )}
       </div>
     );
   }
