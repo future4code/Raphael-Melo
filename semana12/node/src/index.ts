1098bb0d4728d51d02316280c7c0c2a633203a5d
raphaelmelo <contatowbm@gmail.com>
2020-06-23 12:42:05 -0300

    Exercicio 1 - A, B, C

---
+++ semana12/node/src/index.ts
@@ -0,0 +1,22 @@
+// Exercicio 1
+/*
+a - Como fazemos para acessar os parâmetros passados na linha de comando para o node?
+Criamos uma variavel e atribuimos um tipo, essa variavel armazena process.argv[].
+
+b - Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+   
+    const myName: string = process.argv[2];
+    const myAge: number = Number(process.argv[3]);
+
+    console.log(`Olá, ${myName}! Você tem ${myAge} anos.`);
+
+c - c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.   
+  
+    const myName: string = process.argv[2];
+    const myAge: number = Number(process.argv[3]);
+    const em7Anos: number = myAge + 7;
+
+    console.log(`Olá, ${myName}! Você tem ${myAge} anos. Em sete anos você terá ${em7Anos}`);
+
+    */
+
